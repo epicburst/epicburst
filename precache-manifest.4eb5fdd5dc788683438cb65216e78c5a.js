53789a67b6273521179d010bbc54ff70d0b062fc
epicburst <64258120+epicburst@users.noreply.github.com>
2020-04-28 13:16:01 +0200

    Add files via upload

---
+++ precache-manifest.4eb5fdd5dc788683438cb65216e78c5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cea46509225d8f3df60dd83b5a7060",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eb61d129ac472933f8b",
+    "url": "/static/css/main.061ea333.chunk.css"
+  },
+  {
+    "revision": "c3d66e49d245235551d0",
+    "url": "/static/js/2.aa49a3d8.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.aa49a3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb61d129ac472933f8b",
+    "url": "/static/js/main.6c9146b2.chunk.js"
+  },
+  {
+    "revision": "8e3befad3637671e86f3",
+    "url": "/static/js/runtime-main.68e8c8a0.js"
+  },
+  {
+    "revision": "18b60f233db6953cefe3604a568f564b",
+    "url": "/static/media/dos.18b60f23.png"
+  },
+  {
+    "revision": "a29da6c6f8685db84b00bbac73c1b7ac",
+    "url": "/static/media/fondo1.a29da6c6.jpg"
+  },
+  {
+    "revision": "57d6fcda474fae88caebdb86f4cec381",
+    "url": "/static/media/logo.57d6fcda.svg"
+  }
+]);
